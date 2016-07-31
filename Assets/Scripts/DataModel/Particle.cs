53c6ba3fa62327ed4c49ae6b12b69a1f74adf39c
sartaj <sartajcorp@gmail.com>
2016-07-31 13:15:04 +0200

    Description added

--- Assets/Scripts/DataModel/Particle.cs
+++ Assets/Scripts/DataModel/Particle.cs
@@ -2,20 +2,26 @@
 using System.Collections;
 
 public class Particle  {
-    public string name;
-    public double latitude;
-    public double longitude;
-    public bool spawn;
-    public Particle()
-    {
+	public string name;
+	public double latitude;
+	public double longitude;
+	public string charge;
+	public string mass;
+	public string description;
+	public bool spawn;
+	public Particle()
+	{
 
-    }
-    public Particle(string name, double latitude, double longitude)
-    {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        spawn = true;
-    }
+	}
+	public Particle(string name, double latitude, double longitude, string charge, string mass, string description)
+	{
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.charge = charge;
+		this.mass = mass;
+		this.description = description;
+		spawn = true;
+	}
 
 }
